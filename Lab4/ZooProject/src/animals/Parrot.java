62a1138dd746a0baf6f31b079b0e533ef566956a
Eryk7777 <eknap@student.agh.edu.pl>
2025-10-28 17:56:35 +0100

    Add files via upload

---
+++ Lab4/ZooProject/src/animals/Parrot.java
@@ -0,0 +1,17 @@
+package animals;
+
+public class Parrot extends Animal {
+    public Parrot(String name) {
+        super(name, 2);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Parrot named " + name + " with " + legs + " legs";
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Squawk!");
+    }
+}
