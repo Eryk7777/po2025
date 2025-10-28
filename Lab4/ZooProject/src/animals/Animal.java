32054c3cf707e3678ae9593e0fd5b8b6ebcec888
Eryk7777 <eknap@student.agh.edu.pl>
2025-10-28 17:43:46 +0100

    lab4

---
+++ Lab4/ZooProject/src/animals/Animal.java
@@ -0,0 +1,23 @@
+package animals;
+
+public abstract class Animal {
+    protected String name;
+    protected int legs;
+
+    public Animal(String name, int legs) {
+        this.name = name;
+        this.legs = legs;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String getDescription();
+
+    public abstract void makeSound();
+}
