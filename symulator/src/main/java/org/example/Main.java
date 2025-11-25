9a3e3a70c3cb1109238527ff763728464cbd6b02
Eryk7777 <eknap@student.agh.edu.pl>
2025-11-25 16:53:33 +0100

    Update Main.java

--- symulator/src/main/java/org/example/Main.java
+++ symulator/src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import symulator.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Tworzymy komponenty samochodu
+        Sprzeglo sprzeglo = new Sprzeglo("BMW", "Clutch");
+        Silnik silnik = new Silnik("BMW", "V8", 7000);
+        SkrzyniaBiegow skrzynia = new SkrzyniaBiegow("BMW", "Skrzynia Manualna", 6);
+        Pozycja pozycja = new Pozycja(0, 0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Tworzymy samochod
+        Samochod bmw = new Samochod(silnik, skrzynia, pozycja);
+
+        // Testujemy wlaczenie silnika
+        bmw.wlacz();
+        System.out.println("Silnik wlaczony. Obroty: " + silnik.getObroty());
+
+        // Testujemy zmiane biegow
+        skrzynia.zwiekszBieg();
+        System.out.println("Aktualny bieg: " + skrzynia.getAktualnyBieg());
+
+        skrzynia.zmniejszBieg();
+        System.out.println("Aktualny bieg po zmniejszeniu: " + skrzynia.getAktualnyBieg());
+
+        // Testujemy aktualizacje pozycji
+        pozycja.aktualizujPozycje(10, 5);
+        System.out.println("Aktualna pozycja samochodu: " + pozycja.getPozycja());
+
+        // Testujemy wylaczenie silnika
+        bmw.wylacz();
+        System.out.println("Silnik wylaczony. Obroty: " + silnik.getObroty());
+        System.out.println("Bieg po wylaczeniu: " + skrzynia.getAktualnyBieg());
     }
 }
