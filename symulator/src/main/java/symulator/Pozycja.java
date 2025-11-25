c0c130d4fbcb64266721fdd89ea1692c1639ddc7
Eryk7777 <eknap@student.agh.edu.pl>
2025-11-25 16:55:18 +0100

    Update Pozycja.java

--- symulator/src/main/java/symulator/Pozycja.java
+++ symulator/src/main/java/symulator/Pozycja.java
@@ -4,17 +4,32 @@ public class Pozycja {
     private double x;
     private double y;
 
+    // Konstruktor bez argumentów
+    public Pozycja() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    // Konstruktor z argumentami
     public Pozycja(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public void aktualizujPozycję(double deltaX, double deltaY) {
+    public void aktualizujPozycje(double deltaX, double deltaY) {
         this.x += deltaX;
         this.y += deltaY;
     }
 
     public String getPozycja() {
         return "(" + x + ", " + y + ")";
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
     }
 }
