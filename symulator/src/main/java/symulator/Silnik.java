5686e087b9d170c1a7bc6ad6224e9813a1c10e72
Eryk7777 <eknap@student.agh.edu.pl>
2025-11-25 16:56:06 +0100

    Update Silnik.java

--- symulator/src/main/java/symulator/Silnik.java
+++ symulator/src/main/java/symulator/Silnik.java
@@ -4,17 +4,25 @@ public class Silnik extends Komponent {
     private int maxObroty;
     private int obroty;
 
-    public Silnik(String nazwa, double waga, double cena, int maxObroty, int obroty) {
-        super(nazwa, waga, cena);
+    public Silnik(String producent, String model, int maxObroty) {
+        super(producent, model);
         this.maxObroty = maxObroty;
         this.obroty = 0;
     }
 
     public void uruchom() {
-        obroty = 800;
+        obroty = 1000; // wartosc poczatkowa, np. obroty biegu jalowego
     }
 
     public void zatrzymaj() {
         obroty = 0;
+    }
+
+    public int getObroty() {
+        return obroty;
+    }
+
+    public int getMaxObroty() {
+        return maxObroty;
     }
 }
