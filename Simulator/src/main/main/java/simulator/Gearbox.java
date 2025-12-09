147129374c07aa6b408f0d26ef31ba78de1deb64
Eryk7777 <eknap@student.agh.edu.pl>
2025-12-09 14:46:36 +0100

    symulator

---
+++ Simulator/src/main/main/java/simulator/Gearbox.java
@@ -0,0 +1,39 @@
+package java.simulator;
+
+public class Gearbox extends Component {
+    private volatile int gear;
+    private final int numberOfGears;
+
+    public Gearbox(String manufacturer, String model, int weight, int price) {
+        this(manufacturer, model, 6, weight, price);
+    }
+
+    public Gearbox(String manufacturer, String model, int numberOfGears, int weight, int price) {
+        super(manufacturer, model, weight, price);
+        if (numberOfGears < 1) {
+            throw new IllegalArgumentException("numberOfGears must be at least 1");
+        }
+        this.gear = 1;
+        this.numberOfGears = numberOfGears;
+    }
+
+    public void increaseGear() {
+        if (this.gear < numberOfGears) {
+            this.gear++;
+        }
+    }
+
+    public void decreaseGear() {
+        if (this.gear > 1) {
+            this.gear--;
+        }
+    }
+
+    public void setFirstGear() {
+        this.gear = 1;
+    }
+
+    public int getGear() {
+        return this.gear;
+    }
+}
---
+++ Simulator/src/main/main/java/simulator/Position.java
@@ -0,0 +1,29 @@
+package java.simulator;
+
+public class Position {
+    private double x;
+    private double y;
+
+    public Position() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public synchronized void setPosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public synchronized double getX() {
+        return this.x;
+    }
+
+    public synchronized double getY() {
+        return this.y;
+    }
+
+    @Override
+    public synchronized String toString() {
+        return "x: " + this.x + ", y: " + this.y;
+    }
+}
