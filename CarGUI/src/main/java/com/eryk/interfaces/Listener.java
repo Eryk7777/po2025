1fd17a10439a73ad778d7b813f00d91ca9637a9f
Eryk7777 <eknap@student.agh.edu.pl>
2025-12-21 16:29:56 +0100

    Start przebudowy projektu (21.12.2025)

--- CarGUI/src/main/java/com/eryk/interfaces/Listener.java
+++
@@ -1,5 +0,0 @@
-package com.eryk.interfaces;
-
-public interface Listener {
-    void update();
-}
--- CarGUI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.eryk.cargui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.eryk.cargui to javafx.fxml;
-    exports com.eryk.cargui;
-}
